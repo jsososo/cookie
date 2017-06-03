1e07aa37cc969334a06e956fcf15aa07f41450d3
jsososo <jsososo@outlook.com>
2017-06-03 14:34:47 +0800

    修改部分bug，设置和删除结束后可以输入框清空

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
  * Created by soso on 2017/3/30.
  */
 
-var g = document.getElementById('getCookie');
-var s = document.getElementsByClassName('setCookie');
+const g = document.getElementById('getCookie');
+const s = document.getElementsByClassName('setCookie');
+const d = document.getElementById('deleteCookie');
 
-var d = document.getElementById('deleteCookie');
-var r;
+let r;
 //Get cookie
 g.onkeydown = function (e) {
     if (e.keyCode == 13) {
@@ -16,26 +16,30 @@ g.onkeydown = function (e) {
     }
 };
 //Set cookie
-for (var i in s) {
+for (let i in s) {
     s[i].onkeydown = function (e) {
         inputCookie(e.keyCode);
     };
 }
 
 function inputCookie(k) {
-    if (k == 13 ) {
-        if (s[0].value !== undefined) {
-            if (s[1].value !== undefined) {
-                if (s[2].value > 0) {
-                    setCookie(s[0].value, s[1].value, s[2].value *1000);
-                    alert (`Success ! \nCookie's name: ${s[0].value} \nCookie's value: ${s[1].value} \nCookie's expires time: ${s[2].value}s`);
+    if (k === 13 ) {
+        if (s[0].value !== '') {
+            if (s[1].value !== '') {
+                if (!isNaN(s[2].value) && s[2].value > 0) { // is number and bigger than 0
+                    setCookie(s[0].value, s[1].value, s[2].value);
+                    alert (`Setting Success ! \nCookie's name: ${s[0].value} \nCookie's value: ${s[1].value} \nCookie's expires time: ${s[2].value}s`);
+                    [s[0].value, s[1].value, s[2].value] = ['', '', ''];
                 } else {
-                    alert ('input expires time (number)')
+                    s[2].focus();
+                    alert ('input expires time ( >0 )')
                 }
             } else {
+                s[1].focus();
                 alert ('input the value');
             }
         } else {
+            s[0].focus();
             alert ('input the name');
         }
     }
@@ -43,26 +47,24 @@ function inputCookie(k) {
 
 //Delete cookie
 d.onkeydown = function (e) {
-    if (e.keyCode == 13) {
-        if (r = d.value) {
-            DelCookie(r);
-        }
-        console.log(document.cookie);
+    if (e.keyCode == 13 && d.value) {
+        DelCookie(d.value);
     }
 };
 
 function setCookie(name, value, time) {
     //Expiration time
-    var t = new Date();
-    t.setTime(t.getTime() + parseInt(time));
+    let t = new Date();
 
-    document.cookie = name + '=' + escape(value) + ' ;expires=' + t.toGMTString();
+    t.setSeconds(t.getSeconds() + parseInt(time));
+    document.cookie = `${name}=${escape(value)};expires=${t.toGMTString()};`;
 }
 
 function GetCookie(name) {
-    var arr, reg = new RegExp('(^| )' + name + '=([^;]*)(;|$)');
+    let reg = new RegExp('(^| )' + name + '=([^;]*)(;|$)');
+    let arr = document.cookie.match(reg);
 
-    if (arr = document.cookie.match(reg)) {
+    if (arr) {
         return unescape(arr[2]);
     } else {
         alert ('Not Found');
@@ -70,12 +72,14 @@ function GetCookie(name) {
 }
 
 function DelCookie(name) {
-    var t = new Date();
+    let t = new Date();
     t.setTime(-1);
-    var c;
+
+    let c;
 
     if (c = GetCookie(name)) {
-        document.cookie = name + '=' + c + ';expires=' + t.toGMTString() + ';';
+        document.cookie = `${name}=${c};expires=${t.toGMTString()};`;
+        d.value = '';
         alert ('Delete Success !');
     }
 }
